08072a13277fcf8238859bd874b44ebf01662dd1
han <nguyengiahan1803@gmail.com>
2025-11-07 14:14:35 +0700

    Laravel layout + admin

---
+++ restaurants/resources/views/admin/product/product-list.blade.php
@@ -0,0 +1,4 @@
+@extends('layout/admin')
+@section('body')
+<h1>Product</h1>
+@endsection
---
+++ restaurants/routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/service', function () {
+    return view('service');
+})->name('service');
+
+Route::get('/menu', function () {
+    return view('menu');
+})->name('menu');
+
+Route::get('/booking', function () {
+    return view('booking');
+})->name('booking');
+
+Route::get('/team', function () {
+    return view('team');
+})->name('team');
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+})->name('testimonial');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+////////////////////////
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
+
+Route::get('/admin/category', function () {
+    return view('admin//category/category-list');
+})->name('category');
+
+
+Route::get('/admin/product', function () {
+    return view('admin/product/product-list');
+})->name('product');
